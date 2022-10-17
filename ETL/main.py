42f08e53d1c34aea1665cb589e33780775a4c144
Ariel Alonso <rariel.alonso@gmail.com>
2022-10-16 23:28:23 -0300

    entrega ejercicio 4

--- ETL/main.py
+++ ETL/main.py
@@ -1,27 +1,47 @@
-# docker run --rm -v $PWD/ETL/csv_files:/csv_files test-tp
+# docker run --rm -v $PWD/csv_files:/csv_files test-tp
+# docker run -it --rm --network nt-pizza -v $PWD/ETL/csv_files:/csv_files test-tp
 
 #libraries
 import pandas as pd
 import os
+from sqlalchemy import create_engine
 
-#path of files
-path_etl=os.getcwd()
-print(path_etl)
+# set variables to connect
+POSTGRES_USER='aalonso'
+POSTGRES_PASS='ITBA'
+POSTGRES_HOST="postgres-db"
+POSTGRES_DB='database_pizza'
+POSTGRES_PORT='5432'
+POSTGRES_SCHEMA='schema_pizza'
 
-# csv_dict
-csv_dict={"order_details":"order_details.xlsx",
+#make string conection to postgres
+str_conection=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+#make conection
+engine=create_engine(str_conection,
+                    connect_args={'options': f'-csearch_path={POSTGRES_SCHEMA}'}
+                    )
+conn = engine.connect()
+
+# csv_dict of 
+csv_dict={"pizza_types":"pizza_types.xlsx",
+"pizzas":"pizzas.xlsx",
 "orders":"orders.xlsx",
-"pizza_types":"pizzas_types.xlsx",
-"pizzas":"pizzas.xlsx"
+"order_details":"order_details.xlsx",
 }
 
 # looping through the csv_dict
 for k,v in csv_dict.items():
-    print(k)
-    print(v)
-
-print(pd.read_excel('csv_files/order_details.xlsx'))
-
-
-
-print('NOOOOOOOOOOOOOOOoooo')
+    print(f"Excel file to DataFrame: {k}")
+    print('/csv_files/'+f'{v}')
+    data=pd.read_excel("/csv_files/"+f"{v}")
+    print(f"Dataframe to Postgres: {k}")
+    data.to_sql(
+    		k,
+        	conn,
+        	index=False,
+        	if_exists='append'
+        )
+conn.close()
+print('Closing connection to Database')
+print('LOADED DATA TO DB ENDED')
--- database/sql/2-ddl.sql
+++ database/sql/2-ddl.sql
@@ -4,7 +4,7 @@ drop table if exists schema_pizza.orders CASCADE;
 create table schema_pizza.orders (
     order_id INT NOT NULL ,
     date DATE,
-    time TIMESTAMP,
+    time TEXT,
     year INT,
     month INT,
     day_of_week INT,
@@ -16,9 +16,9 @@ drop table if exists schema_pizza.pizza_types CASCADE;
 
 create table schema_pizza.pizza_types (
     pizza_type_id VARCHAR(20) NOT NULL,
-    name VARCHAR(20),
+    name VARCHAR(50),
     category VARCHAR(20),
-    ingredients VARCHAR(30),
+    ingredients VARCHAR(100),
     PRIMARY KEY (pizza_type_id)
 );
 
@@ -41,7 +41,7 @@ create table schema_pizza.order_details(
     order_details_id INT NOT NULL,
     order_id INT,
     pizza_id varchar(20),
-    quantiy INT,
+    quantity INT,
     PRIMARY KEY (order_details_id),
     CONSTRAINT fk_order
     FOREIGN KEY (pizza_id) REFERENCES schema_pizza.pizzas(pizza_id) ON DELETE CASCADE,
