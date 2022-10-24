14a026edc497c5ddd3f922e85e129f881c96a443
Ariel Alonso <rariel.alonso@gmail.com>
2022-10-23 23:49:08 -0300

    mejora en end2end.sh

--- end2end.sh
+++ end2end.sh
@@ -1,32 +1,47 @@
 docker-compose down
-
+PID=$!
+wait $PID
 echo "-------------------------------------------"
 echo "Restarting the container of the database"
 echo "-------------------------------------------"
 
 docker-compose up -d
-
+PID=$!
+wait $PID
 
 echo "-------------------------------------------"
 echo "Removing and creating the database"
 echo "-------------------------------------------"
 
-docker exec postgres-db bash /usr/local/bin/data_tables.sh
+docker exec postgres-db bash /usr/local/bin/data_tables.sh &
+PID=$!
+wait $PID
 
 echo "-------------------------------------------"
 echo "Extracting and loading the data to postgres"
 echo "-------------------------------------------"
 
-docker run -t ETL-container -f ETL/Dockerfile .
-docker run --rm --network nt-pizza -v $PWD/ETL/csv_files:/csv_files ETL-container
+cd ETL/
+docker build -t etl-container -f $PWD/Dockerfile . &
+PID=$!
+wait $PID
+cd ..
+docker run --rm --network nt-pizza -v $PWD/ETL/csv_files:/csv_files etl-container &
+PID=$!
+wait $PID
 
 echo "-------------------------------------------"
 echo "Generating the reports"
 echo "-------------------------------------------"
 
-docker run -t report-image -f Reports/Dockerfile .
-docker run --rm --network nt-pizza -v $PWD/Reports/graphs:/graphs report-image
-
+cd Reports/
+docker build -t report-image -f $PWD/Dockerfile . &
+PID=$!
+wait $PID
+cd ..
+docker run --rm --network nt-pizza -v $PWD/Reports/graphs:/graphs report-image &
+PID=$!
+wait $PID
 
 read -p "Want to END the connection to postgres Database? \n Type yes (it is case sensitive) " confirmation
 if [ "$confirmation" = "yes" ]
